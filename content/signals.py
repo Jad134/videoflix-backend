f90e4d4f7ead1c62a4a1e6a4574b03d8c7d47dc6
Jad El Nader <Jadelnader99@gmail.com>
2024-08-14 20:39:19 +0200

    add ffmpeg with errors

--- content/signals.py
+++ content/signals.py
@@ -3,11 +3,14 @@ from django.dispatch import receiver
 from content.models import Video
 from django.db.models.signals import post_save, post_delete
 
+from content.tasks import convert_480p
+
 @receiver(post_save, sender = Video)
 def video_post_save(sender, instance, created, **kwargs):
     print('Video wurde gepeichert')
     if created: 
         print('New Video created')
+        convert_480p(instance.video_file.path)
 
 
 @receiver(post_delete, sender = Video)

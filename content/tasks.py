15de6036911808935758283f6b94c46791f25d6e
Jad El Nader <Jadelnader99@gmail.com>
2024-08-15 18:46:39 +0200

    add ffmpeg and convert to 480 and 720

--- content/tasks.py
+++ content/tasks.py
@@ -1,9 +1,20 @@
 import subprocess 
-
+import os
 
 
 def convert_480p(source):
- target = source + '_480p.mp4'
- cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
- print(f"Auszuführender Befehl: {cmd}")
- subprocess.run(cmd)
+ os.environ['PATH'] += r';C:\Dev\tools\ffmpeg\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin'
+ base, ext = os.path.splitext(source)
+ target = f"{base}_480p{ext}"
+ cmd = r'C:\Dev\tools\ffmpeg\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin\ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
+ subprocess.run(cmd, shell=True, check=True)
+
+
+def convert_720p(source):
+ os.environ['PATH'] += r';C:\Dev\tools\ffmpeg\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin'
+ base, ext = os.path.splitext(source)
+ target = f"{base}_720p{ext}"
+ cmd = r'C:\Dev\tools\ffmpeg\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin\ffmpeg -i "{}" -s  hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
+ subprocess.run(cmd, shell=True, check=True)
+
+ 

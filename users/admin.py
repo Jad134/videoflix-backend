f75d1ccca11666f55294c8392540a0cd2f2981c2
Jad El Nader <Jadelnader99@gmail.com>
2024-08-19 12:59:29 +0200

    create custom user admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from users.forms import CustomUserCreationForm
 from users.models import CustomUser
 
 # Register your models here.
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    add_form = CustomUserCreationForm
+    fieldsets = (
+        
+        (
+            'Individuelle Daten',
+            {
+                'fields': (
+                    'custom',
+                    'phone',
+                    'address'
+                )
+            }
+        ),
+        *UserAdmin.fieldsets,
+    )

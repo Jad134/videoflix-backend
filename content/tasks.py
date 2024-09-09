f3aaf2ff4fb745b6fcb27a78a4a7aa33b950874f
Jad El Nader <Jadelnader99@gmail.com>
2024-09-09 19:30:04 +0200

    test ffmpeg other cmd route

--- content/tasks.py
+++ content/tasks.py
@@ -4,18 +4,18 @@ from content.models import Video
 from django.core.files import File
 
 def convert_480p(source, video_id):
-    os.environ['PATH'] += r';C:\Dev\tools\ffmpeg\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin'
+    # server
     base, ext = os.path.splitext(source)
     target = f"{base}_480p{ext}"
-    cmd = r'C:\Dev\tools\ffmpeg\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin\ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
+    cmd = f'/usr/bin/ffmpeg -i "{source}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{target}"'
     subprocess.run(cmd, shell=True, check=True)
     update_converted_files(video_id)
 
 def convert_720p(source, video_id):
-    os.environ['PATH'] += r';C:\Dev\tools\ffmpeg\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin'
+    # server
     base, ext = os.path.splitext(source)
     target = f"{base}_720p{ext}"
-    cmd = r'C:\Dev\tools\ffmpeg\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin\ffmpeg -i "{}" -s  hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
+    cmd = f'/usr/bin/ffmpeg -i "{source}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{target}"'
     subprocess.run(cmd, shell=True, check=True)
     update_converted_files(video_id)
 

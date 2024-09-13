4a1b0786c6441614c391414689f3ef6388820089
Jad El Nader <Jadelnader99@gmail.com>
2024-09-13 19:04:10 +0200

    change static url for deployment

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -130,8 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/staticfiles')
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/staticfiles')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/videoflix/staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
